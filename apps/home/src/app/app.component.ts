eca5937cd2a5ab92fc7a1e3dfd89ceec243a4ac1
Ian Flynn <patrickianflynn@gmail.com>
2022-05-05 18:51:06 -0400

    update

--- apps/home/src/app/app.component.ts
+++ apps/home/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Message } from '@fe/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  name = 'ian';
   hello$ = this.http.get<Message>('/api/hello');
   constructor(private http: HttpClient) {}
 }
